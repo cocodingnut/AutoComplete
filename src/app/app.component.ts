25d6db5b893d62196479184eccbaf3080317bc8b
Joyce Dai <jingyued@uci.edu>
2024-06-28 11:21:36 -0700

    autocomplete-googleapi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,15 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { UserService } from './shared/services/user.service';
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <h1>Autocomplete</h1>
+    <autocomplete-input (onSelect)="handleSelect($event)"></autocomplete-input>
+  `,
+  styles: [``]
 })
 export class AppComponent {
-  searchForm: FormGroup;
-  results: any[] = [];
-  filteredResults: any[] = [];
-  displayFilteredResults: boolean = false;
-
-  constructor(private fb: FormBuilder, private userService: UserService) {
-    this.searchForm = this.fb.group({
-      username: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9-]+$/)]],
-      filter: ['']
-    });
+  handleSelect(selectedItem: string) {
+    alert(`Selected: ${selectedItem}`);
   }
-
-  onSubmit() {
-    if (this.searchForm.valid) {
-      const username = this.searchForm.value.username;
-      this.userService.searchUsers(username).subscribe(results => {
-        this.results = results.items;
-      });
-    } else {
-      console.error('Invalid username');
-    }
-  }
-  onFilter() {
-    this.displayFilteredResults = true;
-    const filterValue = this.searchForm.value.filter.toLowerCase();
-    // this.filteredResults = this.results.filter(user =>
-    //   Object.values(user).some(val => typeof val === 'string' && val.toLowerCase().includes(filterValue))
-    // );
-    this.filteredResults = this.results.filter(user =>
-      ['id', 'login', 'html_url'].some(key =>
-        user[key].toString().toLowerCase().includes(filterValue)
-      )
-    );
-
-    console.log(this.filteredResults)
-  }
-
-
 }
